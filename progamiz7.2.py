5a9ccc362acc757bc596cb8575821d8003c8dad4
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-07-16 02:40:53 -0700

    Add files via upload

---
+++ progamiz7.2.py
@@ -0,0 +1,11 @@
+number = int (input("enter your number : "))
+if number>0:
+    print("the number is positive ")
+
+elif number<0:
+
+    print("Negative number")
+else:
+    print("the number is zero")
+
+    print("this  code is always executes ")
